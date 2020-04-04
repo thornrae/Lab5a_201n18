82006126c1591f173b8baf286f1b59c759bf2b97
Taylor Thornton <thornrae@gmail.com>
2020-04-04 14:18:15 -0700

    question 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Lab5a_201n18
 Starter Code
 Demo question answered in class
+Question 2 - multiply 
--- app.js
+++ app.js
@@ -28,11 +28,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var multiply = a * b;
+  return [multiply, 'The product of ' + a + ' and ' + b + ' is ' + multiply + '.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -117,7 +118,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
